bb13ed68afc3886827038fc6bad5e1627c9ed2c0
beyondJohn <jpaul@iamxml.com>
2017-11-15 04:30:15 -0500

    added audio

--- lasers.js
+++ lasers.js
@@ -2,6 +2,8 @@ var firerightlaser = function () {
 
     //move right laser
     if (!isrightactive) {
+        var audio = new Audio('space_laser_shot.mp3');
+        audio.play();
         isrightactive = true;
         var rightlaser = document.getElementsByClassName("rightlaser")[0];
         rightlaser.setAttribute("style", "display:block; right:100%; bottom:70px");
@@ -21,23 +23,25 @@ var firerightlaser = function () {
 var isrightactive = false;
 
 var fireleftlaser = function () {
-    
-        //move left laser
-        if (!isleftactive) {
-            isleftactive = true;
-            var leftlaser = document.getElementsByClassName("leftlaser")[0];
-            leftlaser.setAttribute("style", "display:block; left:95%; bottom:88px");
-            setTimeout(function () {
-                leftlaser.removeAttribute("style");
-                leftlaser.setAttribute("style", "display:none; left:-22%; bottom:-31px");
-            }, 400);
-            setTimeout(function () {
-                leftlaser.setAttribute("style", "display:block; left:-22%; bottom:-31px");
-                isleftactive = false;
-            }, 600);
-        }
-    
-    
-    
+
+    //move left laser
+    if (!isleftactive) {
+        var audio = new Audio('space_laser_shot.mp3');
+        audio.play();
+        isleftactive = true;
+        var leftlaser = document.getElementsByClassName("leftlaser")[0];
+        leftlaser.setAttribute("style", "display:block; left:95%; bottom:88px");
+        setTimeout(function () {
+            leftlaser.removeAttribute("style");
+            leftlaser.setAttribute("style", "display:none; left:-22%; bottom:-31px");
+        }, 400);
+        setTimeout(function () {
+            leftlaser.setAttribute("style", "display:block; left:-22%; bottom:-31px");
+            isleftactive = false;
+        }, 600);
     }
-    var isleftactive = false;
+
+
+
+}
+var isleftactive = false;

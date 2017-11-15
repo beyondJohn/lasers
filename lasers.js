956a1174c049fd5b1061ebce4567cd404673e6b2
beyondJohn <jpaul@iamxml.com>
2017-11-15 01:19:58 -0500

    right laser working

--- lasers.js
+++ lasers.js
@@ -1,5 +1,21 @@
 var firelaser = function () {
-    alert('hi');
-    //add line to be used as right laser
+
+    //move right laser
+    if (!isactive) {
+        isactive = true;
+        var rightlaser = document.getElementsByClassName("rightlaser")[0];
+        rightlaser.setAttribute("style", "display:block; right:100%; bottom:70px");
+        setTimeout(function () {
+            rightlaser.removeAttribute("style");
+            rightlaser.setAttribute("style", "display:none; right:-12%; bottom:-54px");
+        }, 400);
+        setTimeout(function () {
+            rightlaser.setAttribute("style", "display:block; right:-12%; bottom:-54px");
+            isactive = false;
+        }, 600);
+    }
+
+
 
 }
+var isactive = false;
